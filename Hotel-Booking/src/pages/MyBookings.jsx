a94f65c0f17c90a1a43172b2e7eaf7c912035b9d
Bhavna kourav <bhavnakourav8@gmail.com>
2025-04-30 12:11:45 +0530

    add files

---
+++ Hotel-Booking/src/pages/MyBookings.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const MyBookings = () => {
+  return (
+    <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32'>
+        <Title title='My Bookings' subTitle='Easily managr your past,current,and upcoming hotel reservations in one place. Plan Your trips seamlessly with just a afew clicks' align='left'/>
+      
+    </div>
+  )
+}
+
+export default MyBookings
